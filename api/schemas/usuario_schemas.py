de1a96d6cc84f0ef92f400367de6b8e0842be5bd
Fernando FCandiotti <fernandocandiotti@xbrain.com.br>
2022-05-28 22:50:42 -0300

    Adiciona schema usuario

---
+++ api/schemas/usuario_schemas.py
@@ -0,0 +1,24 @@
+from api import ma
+from ..models import usuario_model
+from marshmallow import fields
+
+class UsuariosSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = usuario_model.Usuarios
+        load_instance = True
+        fields = ('id', 'nome', 'cpf', 'pis', 'email', 'senha', 'pais', 'estado', 'municipio', 'cep', 'rua', 'numero',
+                  'complemento')
+
+    nome = fields.String(required=True)
+    cpf = fields.String(required=True)
+    pis = fields.String(required=True)
+    email = fields.String(required=True)
+    senha = fields.String(required=True)
+    pais = fields.String(required=True)
+    estado = fields.String(required=True)
+    municipio = fields.String(required=True)
+    cep = fields.String(required=True)
+    rua = fields.String(required=True)
+    numero = fields.Integer(required=True)
+    complemento = fields.String(required=True)
+
