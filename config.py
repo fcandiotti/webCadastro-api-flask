2e031d0592d0eb852ba00a697a65b2381dee9416
Fernando FCandiotti <fernandocandiotti@xbrain.com.br>
2022-05-30 11:12:22 -0300

    Corrige bug db

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DB = 'api_pontotel'
 
 #SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
 
-SQLALCHEMY_DATABASE_URI = 'postgres://nktntuwwjtdvet:2a991977c0d9e564517c99db1c8f27d90dbf263da39423106c125fca7380cd53@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d7l8tfcq54q7hm'
+SQLALCHEMY_DATABASE_URI = 'postgresql://nktntuwwjtdvet:2a991977c0d9e564517c99db1c8f27d90dbf263da39423106c125fca7380cd53@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d7l8tfcq54q7hm'
 #SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY="aplicacao_flask"
